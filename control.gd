5dd8813b5f9448b9682911f5a2a6810e975caca2
Liiiinaaa <carolina.rod5289@gmail.com>
2026-06-08 20:02:15 +0100

    Initial clean Godot project upload

---
+++ control.gd
@@ -0,0 +1,16 @@
+extends Control
+
+const GAME_SCENE = "res://jogo/Game.tscn"
+var _load_progress = []
+
+@onready var video = $Control/VideoStreamPlayer
+
+func _ready():
+	video.play()
+	ResourceLoader.load_threaded_request(GAME_SCENE)
+
+func _process(_delta):
+	var status = ResourceLoader.load_threaded_get_status(GAME_SCENE, _load_progress)
+	if status == ResourceLoader.THREAD_LOAD_LOADED:
+		var scene = ResourceLoader.load_threaded_get(GAME_SCENE)
+		get_tree().change_scene_to_packed(scene)
